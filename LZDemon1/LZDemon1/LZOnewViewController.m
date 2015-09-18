ca2f83583bdda698f02a0077197acbebb665fe35
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-09-18 17:54:48 +0800

    add constraint

--- LZDemon1/LZDemon1/LZOnewViewController.m
+++ LZDemon1/LZDemon1/LZOnewViewController.m
@@ -21,6 +21,32 @@
    
     self.imgView.image = img;
     self.title = @"One";
+    
+    UIButton *pushBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [pushBtn setBackgroundColor:[UIColor redColor]];
+    [pushBtn setTitle:@"Push" forState:UIControlStateNormal];
+    
+    UIButton *presentBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [presentBtn setBackgroundColor:[UIColor blueColor]];
+    [presentBtn setTitle:@"present" forState:UIControlStateNormal];
+    
+    pushBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    presentBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:pushBtn];
+    [self.view addSubview:presentBtn];
+    
+    NSDictionary *constraintDict = @{@"pushBtn":pushBtn, @"presentBtn":presentBtn};
+    NSArray *pushBtnH = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[pushBtn]-20-|" options:0 metrics:nil views:constraintDict];
+    [self.view addConstraints:pushBtnH];
+    
+    NSArray *pushBtnV = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-84-[pushBtn(50)]-20-[presentBtn(==pushBtn)]" options:NSLayoutFormatAlignAllCenterX metrics:nil views:constraintDict];
+    
+    [self.view addConstraints:pushBtnV];
+    
+    NSLayoutConstraint *presentBtnH = [NSLayoutConstraint constraintWithItem:presentBtn attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:pushBtn attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0];
+    [self.view addConstraint:presentBtnH];
+    
 }
 
 
