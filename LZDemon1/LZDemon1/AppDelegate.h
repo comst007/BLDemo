07be518d61a2ea45a313b478fd4cb4e07d8405b5
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-09-18 13:10:16 +0800

    intial

---
+++ LZDemon1/LZDemon1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZDemon1
+//
+//  Created by comst on 15/9/18.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
