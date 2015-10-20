cac2c0704c777e33dd5164c5f15c802eacf24e18
LiuZHao <comst@comstdeMacBook-Pro.local>
2015-10-21 00:09:32 +0800

    ios homework initial

---
+++ LZIOSHomework/LZIOSHomework/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZIOSHomework
+//
+//  Created by comst on 15/10/20.
+//  Copyright (c) 2015年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
